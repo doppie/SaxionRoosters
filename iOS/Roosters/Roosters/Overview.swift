84c2b33ab5581192d2a70e2900666f13f77f5a4f
Hugo Olthof <hugo_olthof@hotmail.com>
2016-10-09 11:40:07 +0200

    Restructured repository and added iOS project

---
+++ iOS/Roosters/Roosters/Overview.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class Overview: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func setBackgroundColor(color: UIColor) {
+        self.view.backgroundColor = color
+    }
+}
---
+++ iOS/Roosters/Roosters/TabBarController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
