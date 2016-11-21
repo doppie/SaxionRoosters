68eaaa770f243045922d107e0bf5deb58d079421
Wessel Perik <info@wesselperik.com>
2016-11-21 20:06:15 +0100

    Wear error-views changed
    When an error occurs whilst loading the data, the wear-app displays now
    an error-message along with a retry-button.

--- Android/wear/src/main/java/dev/saxionroosters/MainActivity.java
+++ Android/wear/src/main/java/dev/saxionroosters/MainActivity.java
@@ -6,7 +6,11 @@ import android.support.v4.content.ContextCompat;
 import android.support.wearable.view.WatchViewStub;
 import android.support.wearable.view.WearableListView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.wearable.DataApi;
@@ -22,6 +26,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import dev.saxionroosters.adapters.CollegeAdapter;
 import dev.saxionroosters.extras.Tools;
@@ -33,6 +38,9 @@ public class MainActivity extends Activity implements WearableListView.ClickList
     private GoogleApiClient googleClient;
     private WatchViewStub stub;
     private ProgressBar progressBar;
+    private ImageView errorImageView;
+    private TextView errorTextView;
+    private Button errorRetryButton;
     private ArrayList<String> titleArray;
     private ArrayList<String> roomArray;
     private ArrayList<String> timeArray;
@@ -65,8 +73,11 @@ public class MainActivity extends Activity implements WearableListView.ClickList
             public void onLayoutInflated(WatchViewStub stub) {
                 listView = (WearableListView) stub.findViewById(R.id.list_view);
                 progressBar = (ProgressBar) stub.findViewById(R.id.progressBar);
+                errorImageView = (ImageView) stub.findViewById(R.id.errorImage);
+                errorTextView = (TextView) stub.findViewById(R.id.errorText);
+                errorRetryButton = (Button) stub.findViewById(R.id.retryButton);
                 loadAdapter();
-                setProgressBarVisible(true);
+                sendDataRequest();
             }
         });
 
@@ -84,11 +95,39 @@ public class MainActivity extends Activity implements WearableListView.ClickList
             // Hide progressbar
             progressBar.setVisibility(View.GONE);
         } else {
+            // Hide error message
+            errorImageView.setVisibility(View.GONE);
+            errorTextView.setVisibility(View.GONE);
+            errorRetryButton.setVisibility(View.GONE);
+
             // Make progressbar visible and change color to white
             progressBar.setVisibility(View.VISIBLE);
             progressBar.setIndeterminate(true);
             progressBar.getIndeterminateDrawable().setColorFilter(0xFFFFFFFF, android.graphics.PorterDuff.Mode.SRC_ATOP);
         }
+    }
+
+    private void setErrorView(String errorText) {
+        // Hide progressbar
+        setProgressBarVisible(false);
+
+        // Show imageview
+        errorImageView.setVisibility(View.VISIBLE);
+        errorImageView.setImageResource(R.mipmap.ic_error);
+
+        // Show error text
+        errorTextView.setVisibility(View.VISIBLE);
+        errorTextView.setText(errorText);
+
+        // Show retry button
+        errorRetryButton.setVisibility(View.VISIBLE);
+        errorRetryButton.setText(getResources().getString(R.string.btn_retry));
+        errorRetryButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendDataRequest();
+            }
+        });
     }
 
     private String formatDate(String inputDate) {
@@ -104,6 +143,18 @@ public class MainActivity extends Activity implements WearableListView.ClickList
         return formatter.format(date);
     }
 
+    private void sendDataRequest() {
+        setProgressBarVisible(true);
+
+        // Send a request to the host (phone/tablet) to get new college-data
+        PutDataMapRequest putDataMapReq = PutDataMapRequest.create("/get").setUrgent();
+        putDataMapReq.getDataMap().putString("refresh", "true");
+        putDataMapReq.getDataMap().putLong("time", new Date().getTime());
+
+        PutDataRequest putDataReq = putDataMapReq.asPutDataRequest().setUrgent();
+        Wearable.DataApi.putDataItem(googleClient, putDataReq);
+        Tools.log("Wear client | Refresh request sent to host device");
+    }
 
     @Override
     public void onClick(WearableListView.ViewHolder viewHolder) {
@@ -124,14 +175,7 @@ public class MainActivity extends Activity implements WearableListView.ClickList
         Tools.log("Wear client | Connected to Google Api Service");
         Wearable.DataApi.addListener(googleClient, this);
 
-        // Send a request to the host (phone/tablet) to get new college-data
-        PutDataMapRequest putDataMapReq = PutDataMapRequest.create("/get").setUrgent();
-        putDataMapReq.getDataMap().putString("refresh", "true");
-        putDataMapReq.getDataMap().putLong("time", new Date().getTime());
 
-        PutDataRequest putDataReq = putDataMapReq.asPutDataRequest().setUrgent();
-        Wearable.DataApi.putDataItem(googleClient, putDataReq);
-        Tools.log("Wear client | Refresh request sent to host device");
     }
 
     @Override
@@ -213,12 +257,7 @@ public class MainActivity extends Activity implements WearableListView.ClickList
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    if (mAdapter != null) {
-                                        mAdapter.removeItems();
-                                        mAdapter.addItem(new College(getResources().getString(R.string.error_no_internet), "", "", "", 0));
-                                        mAdapter.notifyDataSetChanged();
-                                    }
-                                    setProgressBarVisible(false);
+                                    setErrorView(getResources().getString(R.string.error_no_internet));
                                 }
                             });
                             break;
@@ -227,12 +266,7 @@ public class MainActivity extends Activity implements WearableListView.ClickList
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    if (mAdapter != null) {
-                                        mAdapter.removeItems();
-                                        mAdapter.addItem(new College(getResources().getString(R.string.error_unknown), "", "", "", 0));
-                                        mAdapter.notifyDataSetChanged();
-                                    }
-                                    setProgressBarVisible(false);
+                                    setErrorView(getResources().getString(R.string.error_unknown));
                                 }
                             });
                             break;
