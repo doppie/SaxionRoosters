434d066df340858263f12d7900547e7a3cd2f446
Jelle ten Brinke <jmtenbrinke@gmail.com>
2017-05-22 14:30:55 +0200

    Add a MapFragment to show the location of a selected college on a map.

---
+++ Android/app/src/main/java/dev/saxionroosters/collegedetails/map/MapFragment.java
@@ -0,0 +1,81 @@
+package dev.saxionroosters.collegedetails.map;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.kml.KmlLayer;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+
+import dev.saxionroosters.R;
+import dev.saxionroosters.general.S;
+
+/**
+ * Created by Jelle on 23/02/2017.
+ */
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap map;
+    protected SupportMapFragment mapFragment;
+    private KmlLayer mapLayer;
+
+    private LatLng locationCoordinates;
+    private String locationTitle;
+    private Marker locationMarker;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            locationCoordinates = getArguments().getParcelable(S.COORDINATES);
+            locationTitle = getArguments().getString(S.DEPARTMENT);
+        }
+
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        //important, we don't want the user to be able to drag the fragment.
+        //The MapsFragment is intended as a preview of the full map.
+        map.getUiSettings().setScrollGesturesEnabled(false);
+
+        initializeLayer();
+
+        //position the camera
+        if (locationCoordinates != null)
+            map.animateCamera(CameraUpdateFactory.newCameraPosition(CameraPosition.fromLatLngZoom(locationCoordinates, 17)));
+        if (locationCoordinates != null && locationTitle != null) {
+            locationMarker = map.addMarker(new MarkerOptions().position(locationCoordinates).title(locationTitle));
+            locationMarker.showInfoWindow();
+        }
+    }
+
+    /**
+     * Initializes the Google Maps overlay to draw the highlighted/colored Saxion buildings.
+     */
+    private void initializeLayer() {
+        try {
+            mapLayer = new KmlLayer(map, R.raw.saxion_maps, getActivity());
+            mapLayer.addLayerToMap();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Android/app/src/main/java/dev/saxionroosters/general/S.java
@@ -0,0 +1,13 @@
+package dev.saxionroosters.general;
+
+/**
+ * Created by Jelle on 22/05/2017.
+ *
+ * This class holds identifiers that can be used to pass objects in bundles and intents.
+ */
+
+public class S {
+
+    public static final String COORDINATES = "coordinates";
+    public static final String DEPARTMENT = "department";
+}
